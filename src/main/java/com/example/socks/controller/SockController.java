7092ca0265142ecd624b67252e7466d5226337f0
northin <northin@gmail.com>
2023-08-03 16:07:18 +0300

    комментарии к контроллеру

--- src/main/java/com/example/socks/controller/SockController.java
+++ src/main/java/com/example/socks/controller/SockController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,6 +15,12 @@ public class SockController {
 
     private final SockServiceImpl sockService;
 
+    /**
+     * Регистрирует приход носков на склад.
+     *
+     * @param sockDto Объект {@link SockDto} с информацией о носках, которые поступают на склад.
+     * @return ResponseEntity с текстовым сообщением об успешной регистрации или ошибке.
+     */
     @PostMapping("/income")
     public ResponseEntity<String> registerSocksIncome(@RequestBody SockDto sockDto) {
         if (sockService.registerSocksIncome(sockDto)) {
@@ -22,6 +29,12 @@ public class SockController {
         return ResponseEntity.badRequest().body("invalid data");
     }
 
+    /**
+     * Регистрирует отпуск носков со склада.
+     *
+     * @param sockDto Объект {@link SockDto} с информацией о носках, которые уходят со склада.
+     * @return ResponseEntity с текстовым сообщением об успешной регистрации или ошибке.
+     */
     @PostMapping("/outcome")
     public ResponseEntity<String> registerSocksOutcome(@RequestBody SockDto sockDto) {
         if (sockService.registerSocksOutcome(sockDto)) {
@@ -30,6 +43,15 @@ public class SockController {
         return ResponseEntity.badRequest().body("invalid data");
     }
 
+    /**
+     * Получает общее количество носков на складе, соответствующих переданным критериям запроса.
+     *
+     * @param color       Цвет носков, по которому нужно выполнить фильтрацию.
+     * @param operation   Оператор сравнения значения количества хлопка в составе носков.
+     *                    Возможные значения: "moreThan", "lessThan", "equal".
+     * @param cottonPart  Значение процента хлопка в составе носков для сравнения.
+     * @return ResponseEntity с общим количеством носков на складе, удовлетворяющих критериям запроса.
+     */
     @GetMapping
     public ResponseEntity<Integer> getSocksCount(@RequestParam String color,
                                                  @RequestParam String operation,
